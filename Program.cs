14e71adb14f30946afa694ab3a0b5dc3d5b2fd61
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-05-28 23:23:34 +0100

    Add InputParser and IParsingResult

--- Program.cs
+++ Program.cs
@@ -15,10 +15,47 @@ namespace Twitter
         static void Main(string[] args)
         {
             Container = ContainerConfig.Configure();
+            Console.WriteLine("Enter a command. (Hit ENTER to exit)");
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == string.Empty) break;
 
-            
+                var result = InputParser.Parse(input);
+                result.ExecuteResult();
+            }
+        }
+    }
 
+    public class InputParser
+    {
+        public static IParsingResult Parse(string input)
+        {
+            // switch statement expected here
+            // identify a command and return a corresponding result
+            return new PostingResult("Jake", "Hello");
+        }
+    }
+
+    public interface IParsingResult
+    {
+        void ExecuteResult();
+    }
+
+    public class PostingResult : IParsingResult
+    {
+        private readonly string _username;
+        private readonly string _message;
+
+        public PostingResult(string username, string message)
+        {
+            _username = username;
+            _message = message;
         }
 
+        public void ExecuteResult()
+        {
+            Console.WriteLine($"{_username} said {_message}");
+        }
     }
 }
